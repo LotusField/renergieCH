84d658756b1971a6847e6b6ff0397ccc35235bbb
LotusField <thomasdardano@hotmail.ch>
2026-08-28 23:09:45 +0200

    Init

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -14,8 +14,27 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
+    //! Default MainWindow constructor
+    /*!
+    Takes a pointer to a parent class as parameter.
+        \param *parent the pointer to the parent class.
+    */
     MainWindow(QWidget *parent = nullptr);
+
+    //! Destructor
     ~MainWindow();
+
+protected:
+    //! Responds to the quit button by asking if the user really wants to quit.
+    /*!
+    Takes a pointer to the close event as paramter.
+        \param *event the close event requested.
+    */
+    void closeEvent(QCloseEvent *event) override;
+
+private slots:
+    //! Responds to the trigger by opening the repository url in a web browser.
+    void on_actionRepository_triggered();
 
 private:
     Ui::MainWindow *ui;
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,14 +1,49 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QCloseEvent>
+#include <QMessageBox>
+
+#include <QDesktopServices>
+#include <QUrl>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->actionQuit, &QAction::triggered,
+            this, &MainWindow::close);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+    QMessageBox::StandardButton reply;
+
+    reply = QMessageBox::question(
+        this,
+        tr("Quitter"),
+        tr("Voulez vous vraiment quitter ?"),
+        QMessageBox::Yes | QMessageBox::No
+        );
+
+    if (reply == QMessageBox::Yes) {
+        event->accept();
+    } else {
+        event->ignore();
+    }
+}
+
+
+void MainWindow::on_actionRepository_triggered()
+{
+    QDesktopServices::openUrl(
+        QUrl("https://github.com/LotusField/renergieCH")
+        );
 }
